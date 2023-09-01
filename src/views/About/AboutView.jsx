39e7966d00228241302d4dbc9a5f942c3d536a4c
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-09-01 12:16:42 -0300

    feat: create about view

---
+++ src/views/About/AboutView.jsx
@@ -0,0 +1,22 @@
+import { Footer } from "../../components/Footer/Footer";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { Profile } from "../../About/components/Profile";
+import { Skills } from "../../About/components/Skills";
+import { Projects } from "../../About/components/Projects";
+
+export const AboutView = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="bg-gradient-to-r from-[#141e30] to-[#243b55] flex flex-col justify-between">
+        {/* Component profile*/}
+        <Profile />
+        {/* component skill*/}
+        <Skills />
+        {/* component projects*/}
+        <Projects />
+      </div>
+      <Footer />
+    </>
+  );
+};
