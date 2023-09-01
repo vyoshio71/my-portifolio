3ea815694b85612a0e1581253f92d60eeab8bdc9
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-09-01 12:13:48 -0300

    feat: create experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,19 @@
+import "../../index.css";
+export const Experience = () => {
+  return (
+    <div className="flex flex-col lg:mt-0">
+      <div>
+        <h1 className="text-white font-bold text-center lg:text-2xl">
+          Experiências
+        </h1>
+        <p className="text-white text-center lg:text-xl">
+          Experiência acadêmica com criação de Landing Pages. <br />
+          Interfaces WEB e Mobile com React e React Native. <br />
+          Tailwind e Sass para Estilização. <br />
+          API Restful com Node.js e Express.js <br />
+          MongoDB e MySQL. <br />
+        </p>
+      </div>
+    </div>
+  );
+};
